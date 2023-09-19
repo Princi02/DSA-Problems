e70b267cc24187b37f5ac615f734dbf2f6c16ad4
Princi02 <106614029+Princi02@users.noreply.github.com>
2023-09-19 23:02:59 +0530

    Time: 7 ms (14.81%), Space: 46.1 MB (97.41%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,8 +9,7 @@
  *     }
  * }
  */
-import java.util.Map;
-import java.util.HashMap;
+// 1st.
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a=headA;
@@ -22,14 +21,42 @@ public class Solution {
             a=a.next;
         }
         while(b!=null){
-            // map.put(b, map.getOrDefault(b,0)+1);
             if(map.containsKey(b) && map.get(b)>0) return b;
             b=b.next;
         }
-        // for(map.Entry<ListNode,Integer> entry : map.entrySet()){
-        //     if(entry.getValue()!=0) return headA.entry.getKey().val;
-        //     headA=headA.next;
-        // }
         return null;
     }
 }
+    
+    
+    
+    
+// 2nd.
+// import java.util.Map;
+// import java.util.HashMap;
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode a = headA;
+//         ListNode b = headB;
+        
+//         HashMap<ListNode, Integer> map = new HashMap<>();
+        
+//         while (a != null) {
+//             map.put(a, map.getOrDefault(a, 0) + 1);
+//             a = a.next;
+//         }
+        
+//         while (b != null) {
+//             map.put(b, map.getOrDefault(b, 0) + 1);
+//             b = b.next;
+//         }
+        
+//         for (Map.Entry<ListNode, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() > 1) { // Check for nodes with count > 1
+//                 return entry.getKey(); // Return the intersection node
+//             }
+//         }
+        
+//         return null; // No intersection found
+//     }
+// }
