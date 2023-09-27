638646d932ce1a5f0906de9e8dd9b29a3baeca74
Princi02 <106614029+Princi02@users.noreply.github.com>
2023-09-27 08:52:42 +0530

    Time: 3 ms (75.18%), Space: 39.7 MB (87.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,9 +2,21 @@ class Solution {
     public int change(int amount, int[] coins) {
         int combinations[]=new int[amount+1];
         combinations[0]=1;
-        for(int i=0;i<coins.length;i++){
-                for(int j=coins[i];j<amount+1;j++){
-                combinations[j]+=combinations[j-coins[i]];
+        
+        // 1st
+        // for(int i=0;i<coins.length;i++){
+        //         for(int j=coins[i];j<amount+1;j++){
+        //         combinations[j]+=combinations[j-coins[i]];
+        //     }
+        // }
+        // return combinations[amount];
+        
+        // 2nd
+        for(int coin : coins){
+            for(int i=0;i<amount+1;i++){
+                if(i>=coin){
+                    combinations[i]+=combinations[i-coin];
+                }
             }
         }
         return combinations[amount];
