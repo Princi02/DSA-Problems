08941149d489ec338a73cd8162da14fafb970392
Princi02 <106614029+Princi02@users.noreply.github.com>
2023-10-11 20:54:28 +0530

    Time: 36 ms (99.03%), Space: 57.8 MB (48.54%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,25 +1,41 @@
 class Solution {
     public int minOperations(int[] nums) {
-        // Arrays.sort(nums);
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
+//         TreeSet<Integer> set = new TreeSet<Integer>();
+//         for(int i=0;i<nums.length;i++){
+//             set.add(nums[i]);
+//         }
         
-        int s=0;
-        for(int j : set){
-            nums[s]=j;
-            System.out.println(nums[s]+" "+j);
-            s++;     
+//         int s=0;
+//         for(int j : set){
+//             nums[s]=j;
+//             System.out.println(nums[s]+" "+j);
+//             s++;     
+//         }
+//         int ans=nums.length;
+//         for(int i=0,j=0; i<set.size(); ++i){
+//             while(j < set.size() && nums[j] - nums[i] <= nums.length-1){
+//                 ++j;
+//             }
+//             ans = Math.min(ans, nums.length - (j-i));
+//         }
+//         return ans;
+        
+        
+           Arrays.sort(nums);
+        int uniqueLen = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueLen++] = nums[i];
+            }
         }
-        // Arrays.sort(nums);
-        int ans=nums.length;
-        for(int i=0,j=0; i<set.size(); ++i){
-            while(j < set.size() && nums[j] - nums[i] <= nums.length-1){
+       int ans = nums.length;
+        for (int i = 0, j = 0; i < uniqueLen; ++i) {
+            while (j < uniqueLen && nums[j] - nums[i] <= nums.length - 1) {
                 ++j;
             }
-            ans = Math.min(ans, nums.length - (j-i));
+            ans = Math.min(ans, nums.length - (j - i));
         }
+        
         return ans;
     }
 }
