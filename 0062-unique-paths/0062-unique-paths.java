713d5cc14f02138a5f6074b3d4ae5ed04b0d6de1
Princi02 <106614029+Princi02@users.noreply.github.com>
2023-10-30 08:34:28 +0530

    Time: 0 ms (100.00%), Space: 39 MB (83.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,24 +3,28 @@ class Solution {
         
         // 1st method
         
-        int arr[][]=new int[m][n];
-        for(int[] ar : arr){
-            Arrays.fill(ar,1);
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                arr[i][j]=arr[i-1][j]+arr[i][j-1];
-            }
-        }
-        return arr[m-1][n-1];
+        // int arr[][]=new int[m][n];
+        // for(int[] ar : arr){
+        //     Arrays.fill(ar,1);
+        // }
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         arr[i][j]=arr[i-1][j]+arr[i][j-1];
+        //     }
+        // }
+        // return arr[m-1][n-1];
         
         
         // 2nd method
-        
-        
-        
-        
-        
-        
+         int arr[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0)
+                    arr[i][j]=1;
+                else
+                    arr[i][j]=arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        return arr[m-1][n-1]; 
     }
 }
