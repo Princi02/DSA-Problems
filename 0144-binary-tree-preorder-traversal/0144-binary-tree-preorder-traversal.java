0a13bfc79b6bfb053e5f811f4afca55d44120538
Princi02 <106614029+Princi02@users.noreply.github.com>
2024-01-10 14:32:22 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (12.59%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,33 +13,33 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> list=new ArrayList<Integer>();
-//         dfs(root,list);
-//         return list;
-//     }
-//     private void dfs(TreeNode node,List<Integer> list){
-//         if(node==null) return;
-//         list.add(node.val);
-//         dfs(node.left,list);
-//         dfs(node.right,list);
-//     }
-// }
-
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        
         List<Integer> list=new ArrayList<Integer>();
-        if(root==null) return list;
-        Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode temp=stack.pop();
-            list.add(temp.val);
-            if(temp.right!=null) stack.push(temp.right);
-            if(temp.left!=null) stack.push(temp.left);
-        }
+        dfs(root,list);
         return list;
     }
+    private void dfs(TreeNode node,List<Integer> list){
+        if(node==null) return;
+        list.add(node.val);
+        dfs(node.left,list);
+        dfs(node.right,list);
+    }
 }
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+        
+//         List<Integer> list=new ArrayList<Integer>();
+//         if(root==null) return list;
+//         Stack<TreeNode> stack=new Stack<>();
+//         stack.push(root);
+//         while(!stack.isEmpty()){
+//             TreeNode temp=stack.pop();
+//             list.add(temp.val);
+//             if(temp.right!=null) stack.push(temp.right);
+//             if(temp.left!=null) stack.push(temp.left);
+//         }
+//         return list;
+//     }
+// }
